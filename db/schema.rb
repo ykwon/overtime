04ce5fe1b3790ccae1161a427ab024b4afbda54d
Younghee Kwon <youngheekwon@ykwon.local>
2019-07-09 15:13:38 -0700

    Used the resource generator for posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_09_192036) do
+ActiveRecord::Schema.define(version: 2019_07_09_215856) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.date "date"
+    t.text "rationale"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe "Creationg" do 
+    before do
+      @post = Post.create(date: Date.today , rationale:"anything")
+    end
+    it 'can be created' do       
+      expect(@post).to be_valid
+    end
+
+    it 'cannot be created without date and rantionale' do
+      @post.date = nil 
+      @post.rationale = nil
+      expect(@post).to_not be_valid
+    end
+  end
+end
